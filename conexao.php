a78cfb9c3a9453e10a6a49445f2c5731b691c9dd
mununga <edelsonmununga1234@gmail.com>
2016-10-28 16:26:24 -0800

    fx

--- conexao.php
+++ conexao.php
@@ -1,3 +1,3 @@
 <?php
-$conexao = mysqli_connect("localhost", "root", "") or die("Erro de conexao");
-$db = mysqli_select_db($conexao,"igreja") or die ("erro ao selecionar banco d   e dados");
+//$conexao = mysqli_connect("localhost", "root", "") or die("Erro de conexao");
+//$db = mysqli_select_db($conexao,"igreja") or die ("erro ao selecionar banco d   e dados");
